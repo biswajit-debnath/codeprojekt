fe1aa3ad67aa27f1b5c10dfc125d63b3d192b6a4
biswajit-debnath <biswajitdebnath405@gmail.com>
2025-02-18 10:21:15 +0530

    Feature: Mobile responsiveness added for home page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: { unoptimized: true },
+  // basePath: '/codeprojekt-demo',
+  // assetPrefix: '/codeprojekt-demo/',
+  
 };
+
+// Note: When using custom domains, you need to set the basePath and assetPrefix to empty strings.
 
 export default nextConfig;
