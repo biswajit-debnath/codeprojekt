15c2ef55c279ef10bf285a21d7228ae903247691
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2025-06-08 03:01:14 +0530

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,8 @@ const eslintConfig = [
     files: ["**/*.ts", "**/*.tsx"],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-implicit-any": "off",
     },
   },
 ];
